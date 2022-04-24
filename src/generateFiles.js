18debf842a92b27eef2493aadb301b4964c972b4
Malik <malikspruill@gmail.com>
2022-04-24 02:10:24 -0400

    Added files for the ability to generate an html file and move the css file to the dist folder

---
+++ src/generateFiles.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+let writeFile = htmlContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./dist/index.html", htmlContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: "html file created!"
+            })
+        })
+    })
+}
+
+
+let renameFile = cssContent => {
+    return new Promise((resolve, reject) => {
+        fs.rename("./style.css", "./dist/style.css", err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: "css file sent!"
+            })
+        })
+    })
+}
